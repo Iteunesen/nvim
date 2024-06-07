1f7e83e5a0ee4fe4fd82e0c2a2a80cde1dedbb1f
Ivan Teunesen <ivan.teunesen@inspiro.nl>
2024-06-07 09:32:44 +0200

    Addedd CPP LSP

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -23,3 +23,8 @@ lspconfig.hls.setup({
   capabilities = capabilities,
   filetypes = { "haskell", "lhaskell", "cabal" }
 })
+
+lspconfig.clangd.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
